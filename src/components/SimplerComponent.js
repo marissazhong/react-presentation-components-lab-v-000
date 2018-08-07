f620e5ddfec2c04d20c205bc00dae3e2961297b4
Marissa Zhong <mwz2101@columbia.edu>
2018-08-07 16:20:52 +0000

    automatically backed up by learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,10 @@
 // Code SimplerComponent Here
+
+const SimplerComponent = (props) =>
+  <div>I am just happy.</div>
+  <button onClick={props.handleClick}
+  <input
+    className="field field-light"
+    onChange={props.onChange}
+    limit={props.limit || defaultLimit}
+  />;
